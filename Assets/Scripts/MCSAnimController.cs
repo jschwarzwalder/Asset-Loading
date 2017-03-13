4dfca47a6237fedd5f29214d7fac2e89c182d549
Jami <jamischwarzwalder@gmail.com>
2017-03-12 20:31:09 -0700

    Added Female Main Character

---
+++ Assets/Scripts/MCSAnimController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCSAnimController : MonoBehaviour {
+
+	private Animator anim;
+	private float walking;
+	private float turning;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		walking = 0.0f;
+		turning = 0.0f;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		walking = Input.GetAxis ("Vertical");
+		anim.SetFloat ("walking", walking);
+		turning = Input.GetAxis ("Horizontal");
+		transform.Rotate(new Vector3(0.0f, turning));
+	}
+}
